622b08ec8c3706e94128c9b80b4645ce0bd54b91
tauri <tauri139@tlu.ee>
2017-04-26 06:23:23 +0300

    Andmebaasi abil loodud mudel.

---
+++ DemoMVC/src/DemoMVC/Models/ActorDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DemoMVC.Models
+{
+    public partial class ActorDbContext : DbContext
+    {
+        public virtual DbSet<Actors> Actors { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=ActorDb; Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
