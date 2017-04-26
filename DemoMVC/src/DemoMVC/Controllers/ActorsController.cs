16fda4474b63bac2e5d3cc9838cba60a99075148
tauri <tauri139@tlu.ee>
2017-04-26 07:09:49 +0300

    Kontrollerid lisatud.

---
+++ DemoMVC/src/DemoMVC/Controllers/ActorsController.cs
@@ -0,0 +1,44 @@
+﻿using DemoMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoMVC.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private ActorDbContext _context;
+        public ActorsController(ActorDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Actors.ToList());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Actors actors)
+        {
+            //kui mudeli olek on kehtiv siis
+            if (ModelState.IsValid)
+            {
+                //lisateks näitlejad
+                _context.Actors.Add(actors);
+                //salvestatakse
+                _context.SaveChanges();
+                //suunatakse tagasi Indexi lehele
+                return RedirectToAction("Index");
+            }
+            //kui pole kehtiv, siis tagastame näitlejate vaate
+            return View(actors);
+        }
+    }
+}
